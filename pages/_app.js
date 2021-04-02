6c19a285bdb3c1943140d7357f93ada1c87baa55
Francis Pham <francispham89@gmail.com>
2021-04-01 20:26:17 -0700

    Implemented NProgress, & Fixed Styled Component Flicker on Server Render

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,16 @@
+import NProgress from 'nprogress'; // https://ricostacruz.com/nprogress/
+import Router from 'next/router';
 import Page from '../components/Page';
+
+// Custom NProgress:
+import '../components/styles/nprogress.css';
+/* Default: 
+import 'nprogress/nprogress.css';
+*/
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
