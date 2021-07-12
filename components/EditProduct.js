bd1bbfcb7a9e3cf7bd6942ee82522f9ec606d27a
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-07-12 14:27:02 -0700

    Editing Items II: Implemented GraphQL Mutation to Edit the Product

--- components/EditProduct.js
+++ components/EditProduct.js
@@ -1,8 +1,28 @@
-import { useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 // https://nextjs.org/docs/api-reference/next/head
 import Head from 'next/head';
+import ErrorMessage from "./ErrorMessage";
 import { SINGLE_PRODUCT_QUERY, ProductStyles } from './SingleProduct';
+
+const EDIT_PRODUCT_MUTATION = gql`
+mutation EDIT_PRODUCT_MUTATION (
+  $id: ID!
+  $name: String
+  $description: String
+  $price: Int
+) {
+  editProduct(
+    id: $id
+    data: { id: $id, name: $name, description: $description, price: $price }
+  ) {
+    id
+    name
+    description
+    price
+  }
+}
+`;
 
 export default function EditProduct({ id }) {
   //  ? 1. Getting the Existing Product from query.id in EditPage
@@ -11,13 +31,30 @@ export default function EditProduct({ id }) {
   });
   console.log('Data: ', data, 'Loading: ', loading, 'Error: ', error);
 
-  //  TODO: 2. Using GraphQL Mutation to Edit the Product
+
+  //  ? 2. Using GraphQL Mutation to Edit the Product
+  const [
+    editProduct,
+    { data: editData, loading: editLoading, error: editError },
+  ] = useMutation(EDIT_PRODUCT_MUTATION, {
+    variables: { id },
+    // TODO: Pass In Edited Product here!
+  });
+  console.log('editProduct:', editProduct);
+  console.log('Edit Data: ', editData, 'Edit Loading: ', editLoading, 'Edit Error: ', editError);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+
+  const { Product: { name, description, photo } } = data;
+  console.log('data:', data);
+
   //  TODO: 3. Create State for Form Input
   //  TODO: 4. Create Form to Handle Edit
   return (
     <ProductStyles>
       <Head>
-        <title>Ecommerce</title>
+        <title>Ecommerce | Editing {name}</title>
       </Head>
       <p>EDITING PRODUCT ID: {id}</p>
     </ProductStyles>
