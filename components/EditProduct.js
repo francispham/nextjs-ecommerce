09397720895327ef014196476aa2c451bb20472a
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-07-12 15:35:18 -0700

    Editing Items III: Created Edit Form and Handling Submit with updateProduct Mutation

--- components/EditProduct.js
+++ components/EditProduct.js
@@ -2,8 +2,11 @@ import { gql, useMutation, useQuery } from "@apollo/client";
 
 // https://nextjs.org/docs/api-reference/next/head
 import Head from 'next/head';
-import ErrorMessage from "./ErrorMessage";
+
 import { SINGLE_PRODUCT_QUERY, ProductStyles } from './SingleProduct';
+import ErrorMessage from "./ErrorMessage";
+import useForm from '../lib/useForm';
+import Form from "./styles/Form";
 
 const EDIT_PRODUCT_MUTATION = gql`
 mutation EDIT_PRODUCT_MUTATION (
@@ -12,9 +15,9 @@ mutation EDIT_PRODUCT_MUTATION (
   $description: String
   $price: Int
 ) {
-  editProduct(
+  updateProduct(  # Check for Mutations: http://localhost:3000/api/graphql
     id: $id
-    data: { id: $id, name: $name, description: $description, price: $price }
+    data: { name: $name, description: $description, price: $price }
   ) {
     id
     name
@@ -29,34 +32,77 @@ export default function EditProduct({ id }) {
   const { data, loading, error } = useQuery(SINGLE_PRODUCT_QUERY, {
     variables: { id }
   });
-  console.log('Data: ', data, 'Loading: ', loading, 'Error: ', error);
-
 
   //  ? 2. Using GraphQL Mutation to Edit the Product
   const [
-    editProduct,
+    updateProduct,
     { data: editData, loading: editLoading, error: editError },
-  ] = useMutation(EDIT_PRODUCT_MUTATION, {
-    variables: { id },
-    // TODO: Pass In Edited Product here!
-  });
-  console.log('editProduct:', editProduct);
+  ] = useMutation(EDIT_PRODUCT_MUTATION);
   console.log('Edit Data: ', editData, 'Edit Loading: ', editLoading, 'Edit Error: ', editError);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <ErrorMessage error={error} />;
 
-  const { Product: { name, description, photo } } = data;
-  console.log('data:', data);
-
-  //  TODO: 3. Create State for Form Input
-  //  TODO: 4. Create Form to Handle Edit
+  //  ? 3. Create State for Form Input
+  const { inputs: { name, description, price }, handleChange } = useForm(data?.Product);
+  //  ? 4. Create Form to Handle Edit
   return (
     <ProductStyles>
       <Head>
         <title>Ecommerce | Editing {name}</title>
       </Head>
       <p>EDITING PRODUCT ID: {id}</p>
+      <Form onSubmit={async (e) => {
+        e.preventDefault();
+
+        // ? Handle Submit:
+        await updateProduct({
+          variables: { 
+            id,
+            name,
+            description,
+            price,
+          },
+        }).catch(console.error);
+      }}>
+        <ErrorMessage error={editError} />
+        <fieldset disabled={editLoading} aria-busy={editLoading}>
+          <label htmlFor='name'>
+            Name
+            <input 
+              type='text'
+              id='name'
+              name='name'
+              placeholder='Name'
+              value={name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor='price'>
+            Price
+            <input 
+              type='number'
+              id='price'
+              name='price'
+              placeholder='Price'
+              value={price}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor='description'>
+            Description
+            <textarea 
+              type='text'
+              id='description'
+              name='description'
+              placeholder='Description'
+              value={description}
+              onChange={handleChange}
+            />
+          </label>
+        </fieldset>
+        <button type='submit'>Edit Product 📝 </button>
+      </Form>
     </ProductStyles>
   );
 };
