1a903eaaae8dcb0e4011c2bf3d067f0fadeb3dc6
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-07-16 17:10:31 -0700

    Authentication III: Implemented GraphQL Mutation for Signing In

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,21 +1,57 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
 import Form from './styles/Form';
 import useForm from '../lib/useForm';
+import ErrorMessage from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          email
+          name
+        }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
+    }
+  }
+`;
 
 export default function SignIn() {
-  const { inputs, handleChange } = useForm({
+  const { inputs, handleChange, resetForm } = useForm({
     email: '',
     password: ''
-  })
+  });
 
-  function handleSubmit(e) {
+  const [signin, { data, error, loading }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    // ? Refetch   the currently logged in User
+    refetchQueries: [{ query: CURRENT_USER_QUERY }]
+  });
+  const errorMessage = data?.authenticateUserWithPassword.__typename ===
+  'UserAuthenticationWithPasswordFailure'
+    ? data?.authenticateUserWithPassword : undefined;
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log(inputs);
+
+    // ? Send the Email and Password to the graphQL API
+    await signin();
+    resetForm();
   };
 
   return (
     <Form method='POST' onSubmit={handleSubmit}>  {/* POST Method prevents password showed on url! */}
       <h2>Sign Into Your Account</h2>
-      <fieldset>
+      <ErrorMessage error={errorMessage} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor='email'>
           Email
           <input
