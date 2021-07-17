f02c99bdc3f7f2849d7bd966a21c26d3c9697afa
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-07-17 11:31:22 -0700

    Authentication IV: Implemented GraphQL Mutation for Signing Out

--- components/Nav.js
+++ components/Nav.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 export default function Nav() {
   const user = useUser();
-  console.log('user:', user);
 
   return (
     <NavStyles>
@@ -15,6 +15,7 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
     </NavStyles>
