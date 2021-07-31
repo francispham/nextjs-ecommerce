95d3f089a138f9c36595aeee51da47dbc95cb310
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-07-30 17:42:26 -0700

    Rebuilt Pagination Component with Tailwind

--- components/Pagination.js
+++ components/Pagination.js
@@ -15,7 +15,7 @@ export const PAGINATION_QUERY = gql`
   }
 `;
 
-export default function Pagination({ page }) {
+function oldPagination({ page }) {
   const { error, loading, data } = useQuery(PAGINATION_QUERY);
 
   if(loading) return <p>Loading...</p>;
@@ -39,3 +39,37 @@ export default function Pagination({ page }) {
     </PaginationStyles>
   );
 };
+
+function Pagination({ page }) {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+
+  if(loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count/perPage);
+
+  return (
+    <div className="inline-grid grid-cols-4 gap-x-4 text-center content-center items-stretch justify-center text-lg my-8  h-16 max-w-xl rounded-lg border">
+      <Head>
+        <title>Sick Fits - Page {page} of {pageCount}</title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page <= 1}>← Prev</a>
+      </Link>
+      <span className="relative">
+        <span className="absolute top-0 left-0 w-0.5 h-full bg-gray-200"/>
+        <p>Page {page} of {pageCount}</p>
+        <span className="absolute top-0 right-0 w-0.5 h-full bg-gray-200"/>
+      </span>
+      <span className="relative">
+        <p>{count} Items Total</p>
+        <span className="absolute top-0 -right-3 w-0.5 h-full bg-gray-200"/>
+      </span>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Next →</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Pagination;
